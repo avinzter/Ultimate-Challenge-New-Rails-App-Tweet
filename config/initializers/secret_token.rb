cbf21628f81edbd1570c8e4eaac084fb06f7a478
Geniva B. Lacara <geniva.lacara@gmail.com>
2013-11-04 14:33:07 -0600

    First commit create files for tweet ultimate challenge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAppTweetChallenge::Application.config.secret_token = 'd68cc4d43d0253787a80bdd2958a1f9db695d622bc97305a56a24e950f08fe261fcc8fed05407f25a6f33cf16147a32da0f16b3becf2b5d7dadebeff49fdc56f'
